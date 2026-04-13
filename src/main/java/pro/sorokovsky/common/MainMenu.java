158c9d20ae9d5627b38010ef3852144c8df9226c
Sorokovsky <Sorokovskys@ukr.net>
2026-04-13 23:24:36 +0300

    refactor main menu

--- src/main/java/pro/sorokovsky/common/MainMenu.java
+++ src/main/java/pro/sorokovsky/common/MainMenu.java
@@ -2,8 +2,10 @@ package pro.sorokovsky.common;
 
 import pro.sorokovsky.level1.Level1Frame;
 import pro.sorokovsky.level2.Level2Frame;
+import pro.sorokovsky.level3.Level3Frame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainMenu extends JFrame {
     private final JPanel panel = new JPanel();
@@ -25,21 +27,19 @@ public class MainMenu extends JFrame {
     }
 
     private void setupButtons() {
-        panel.add(setupLevelOneButton());
-        panel.add(setupLevelTwoButton());
+        panel.add(generateButton("Рівень 1", Level1Frame::new));
+        panel.add(generateButton("Рівень 2", Level2Frame::new));
+        panel.add(generateButton("Рівень 3", Level3Frame::new));
     }
 
-    private JButton setupLevelOneButton() {
-        final var button = new JButton();
-        button.setText("Рівень 1");
-        button.addActionListener(_ -> new Level1Frame().setVisible(true));
+    private JButton generateButton(String text, FrameSupplier supplier) {
+        final var button = new JButton(text);
+        button.addActionListener(_ -> supplier.get().setVisible(true));
         return button;
     }
 
-    private JButton setupLevelTwoButton() {
-        final var button = new JButton();
-        button.setText("Рівень 2");
-        button.addActionListener(_ -> new Level2Frame().setVisible(true));
-        return button;
+    @FunctionalInterface
+    private interface FrameSupplier {
+        Frame get() throws HeadlessException;
     }
 }
---
+++ src/main/java/pro/sorokovsky/level3/Level3Frame.java
@@ -0,0 +1,11 @@
+package pro.sorokovsky.level3;
+
+import javax.swing.*;
+
+public class Level3Frame extends JFrame {
+    public Level3Frame() {
+        setTitle("Рівень 3");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(800, 600);
+    }
+}
