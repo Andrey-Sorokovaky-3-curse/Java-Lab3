d760d95d1043cd22577f1328b331d6dbdc310113
Sorokovskys <Sorokovskys@ukr.net>
2026-04-14 15:27:08 +0300

    feature form for create one array

--- src/main/java/pro/sorokovsky/level3/Level3Frame.java
+++ src/main/java/pro/sorokovsky/level3/Level3Frame.java
@@ -1,11 +1,25 @@
 package pro.sorokovsky.level3;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Level3Frame extends JFrame {
     public Level3Frame() {
         setTitle("Рівень 3");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setSize(800, 600);
+        setupComponents();
+    }
+
+    private void setupComponents() {
+        final var panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        final var label = new JLabel("Введіть кількість елементів одновимірного масиву: ");
+        final var input = new JTextField(10);
+        final var createOneArray = new JButton("Створити одновимірний масив");
+        panel.add(label);
+        panel.add(input);
+        panel.add(createOneArray);
+        add(panel);
     }
 }
