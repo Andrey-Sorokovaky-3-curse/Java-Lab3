d7cc88808e947870708e261f355ea2d49ea7c247
Sorokovskys <Sorokovskys@ukr.net>
2026-04-14 13:36:14 +0300

    fix: running

--- src/main/java/pro/sorokovsky/Main.java
+++ src/main/java/pro/sorokovsky/Main.java
@@ -5,7 +5,7 @@ import pro.sorokovsky.common.MainMenu;
 import javax.swing.*;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         final var frame = new MainMenu();
         frame.setVisible(true);
     }
